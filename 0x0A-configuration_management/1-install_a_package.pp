79f4a2c7f16d7924914968ea8d252d358c9fe2b3
Shiftyshadows <purple.quasar@proton.me>
2023-12-06 00:05:26 +0300

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,8 +5,18 @@ package { 'python3-pip':
   ensure => installed,
 }
 
+package { 'libffi-dev':
+  ensure => installed,
+}
+
+exec { 'install_dependencies':
+  command => '/usr/bin/pip3 install -U setuptools wheel',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
+}
+
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => ['/usr/bin'],
-  require => Package['python3-pip'],
+  require => [Package['python3-pip'], Package['libffi-dev'], Exec['install_dependencies']],
 }
