e7111bde6dd8e7069ea31f902da1ff15a482f036
Shiftyshadows <purple.quasar@proton.me>
2023-12-06 02:50:27 +0300

    2. Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,41 +2,58 @@
 # Define a class for configuring the custom HTTP header
 class custom_http_response_header {
   
-  # Install Nginx package
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Create custom Nginx configuration file
-  file { '/etc/nginx/sites-available/0-custom_http_response_header':
-    content => '
-server {
-    listen 80;
-    server_name _;
-
-    location / {
-        add_header X-Served-By $host;
-        # Additional configuration if needed
+    # Install Nginx package
+    package { 'nginx':
+        ensure => installed,
     }
 
-    # Additional server blocks or configuration if needed
+    # Create custom Nginx configuration file
+    file { '/etc/nginx/sites-available/default':
+        content => 'error_log  /var/log/nginx/error.log notice;
+pid        /var/run/nginx.pid;
+
+events {
+    # Default event-related configurations
+}
+
+http {
+    server {
+        listen 80;
+        server_name 100.26.220.1;
+
+        location /redirect_me {
+            return 301 https://www.example.com/;
+        }
+
+        location / {
+            root /var/www/html/;
+            index index.html;
+            add_header X-Served-By $host;
+        }
+
+        error_page 404 /404.html;
+        location = /404.html {
+            root /var/www/html;
+            internal;
+        }
+    }
 }
 ',
-    notify  => Service['nginx'],
-  }
+        notify  => Service['nginx'],
+    }
 
-  # Create a symbolic link
-  file { '/etc/nginx/sites-enabled/0-custom_http_response_header':
-    ensure => link,
-    target => '/etc/nginx/sites-available/0-custom_http_response_header',
-  }
+    # Create a symbolic link
+    file { '/etc/nginx/sites-enabled/default':
+        ensure => link,
+        target => '/etc/nginx/sites-available/default',
+    }
 
-  # Ensure Nginx service is running and enabled
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => File['/etc/nginx/sites-available/0-custom_http_response_header'],
-  }
+    # Ensure Nginx service is running and enabled
+    service { 'nginx':
+        ensure  => running,
+        enable  => true,
+        require => File['/etc/nginx/sites-available/default'],
+    }
 }
 
 # Include the custom_http_response_header class
