295a9ed181d8972b5a02f015bf3b16cf2d830773
Shiftyshadows <purple.quasar@proton.me>
2023-12-06 00:11:12 +0300

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
+# Install a specific version of flask (2.1.0)
 
 package { 'python3-pip':
   ensure => installed,
@@ -15,8 +15,14 @@ exec { 'install_dependencies':
   require => Package['python3-pip'],
 }
 
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.0.3',
+  path    => ['/usr/bin'],
+  require => [Package['python3-pip'], Package['libffi-dev'], Exec['install_dependencies']],
+}
+
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => ['/usr/bin'],
-  require => [Package['python3-pip'], Package['libffi-dev'], Exec['install_dependencies']],
+  require => [Package['python3-pip'], Package['libffi-dev'], Exec['install_dependencies'], Exec['install_werkzeug']],
 }
