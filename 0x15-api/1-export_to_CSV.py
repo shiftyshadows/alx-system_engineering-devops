8182f85698ba2de3385fd7d33df4cc20ec915fdd
Shiftyshadows <purple.quasar@proton.me>
2024-01-03 05:33:17 +0300

    1. Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,7 +7,6 @@ import requests
 import sys
 import csv
 
-
 def get_employee_todo_progress(employee_id):
     """ This is a function definition"""
     api_url = "https://jsonplaceholder.typicode.com/"
@@ -22,37 +21,43 @@ def get_employee_todo_progress(employee_id):
         cm_tasks = []
         for td in tds:
             if td.get("completed"):
-                cm_tasks.append(td)
+                cm_tasks.append(td.get("title"))
         num_cm_tasks = len(cm_tasks)
         num_tasks = len(tds)
-
-        csv_filename = "{}.csv".format(employee_id)
-        with open(csv_filename, mode='w', newline='') as csv_file:
-            fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-            writer.writeheader()
-            for task in cm_tasks:
-                writer.writerow({
-                    'USER_ID': employee_id,
-                    'USERNAME': e_name,
-                    'TASK_COMPLETED_STATUS': task.get("completed"),
-                    'TASK_TITLE': task.get("title")
-                })
-
         print("Employee {} is done with tasks({}/{}):".format(
             e_name, num_cm_tasks, num_tasks))
         for task in cm_tasks:
-            print("\t {}".format(task.get("title")))
-        print("Data exported to {}".format(csv_filename))
+            print("\t {}".format(task))
     else:
         print("Error: Unable to fetch TODO list for employee {}".format(
             employee_id))
+
+def write_to_csv(employee_id):
+    """ This is a function definition"""
+    api_url = "https://jsonplaceholder.typicode.com/"
+    api_user = "{}users/{}".format(api_url, employee_id)
+    api_user_td = "{}todos".format(api_url)
+    u_response = requests.get(api_user)
+    td_response = requests.get(api_user_td, params={"userId": employee_id})
+    if td_response.status_code == 200:
+        tds = td_response.json()
+        e_name = u_response.json().get("username")
+        with open("{}.csv".format(employee_id), "w", newline="") as csvfile:
+            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            [writer.writerow(
+                [employee_id, e_name, td.get("completed"), td.get("title")]
+             ) for td in tds]
+
+    else:
+        print("Error: Unable to fetch TODO list for employee {}".format(
+            employee_id))
+
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
     else:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
+        e_id = int(sys.argv[1])
+#        get_employee_todo_progress(e_id)
+        write_to_csv(e_id)
