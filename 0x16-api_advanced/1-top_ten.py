9901fd45deb92e2c5ec45537c958fb6e9e93ef82
Shiftyshadows <purple.quasar@proton.me>
2024-09-13 19:14:17 +0300

    1. Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,6 +3,7 @@
     prints the titles of the first 10 hot posts listed for
     a given subreddit.
 """
+import requests
 
 
 def top_ten(subreddit):
@@ -10,14 +11,15 @@ def top_ten(subreddit):
         Queries the Reddit API and returns the top 10 hot posts
         of the subreddit.
     """
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    header = {
+        "User-Agent": (
+            "Python:subreddit.subscriber.counter:v1.0 (by /u/shifty_shadows)"
+        )
+    }
+    sub_info = requests.get(url, headers=header, allow_redirects=False)
     if sub_info.status_code >= 300:
         print('None')
     else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+        for child in sub_info.json().get("data").get("children"):
+            print(child.get("data").get("title"))
