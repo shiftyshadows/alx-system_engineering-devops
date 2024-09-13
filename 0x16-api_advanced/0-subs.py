f3e8a2b88ee9e35b92982d15bb1e2705d1a8044d
Shiftyshadows <purple.quasar@proton.me>
2024-09-13 18:27:51 +0300

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,13 +19,8 @@ def number_of_subscribers(subreddit):
     headers = {
             "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code == 404:
-            return 0
-
-        result = response.json().get("data")
-        return result.get("subscribers")
-    except requests.RequestException:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    result = response.json().get("data")
+    return result.get("subscribers")
