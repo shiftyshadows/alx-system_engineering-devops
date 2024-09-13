3313a8ef06dd2f1d919b9e1f430c44ec30d3b222
Shiftyshadows <purple.quasar@proton.me>
2024-09-13 18:25:44 +0300

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,19 +15,17 @@ def number_of_subscribers(subreddit):
     Returns:
         int: Number of subscribers or 0 if the subreddit is invalid.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        'User-Agent': (
-            'Python:subreddit.subscriber.counter:v1.0 (by /u/yourusername)'
-        )
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
 
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
+        if response.status_code == 404:
             return 0
+
+        result = response.json().get("data")
+        return result.get("subscribers")
     except requests.RequestException:
         return 0
