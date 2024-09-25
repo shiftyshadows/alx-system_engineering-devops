254fe6f60d076780b12820eb8b7b20c86bb0a039
Shiftyshadows <purple.quasar@proton.me>
2024-09-25 06:11:57 +0300

    2. If only you could see what I've seen with your eyes

--- 0x14-mysql/create-db.sql
+++ 0x14-mysql/create-db.sql
@@ -1,13 +1,18 @@
 -- Create db on the master server
 CREATE DATABASE IF NOT EXISTS tyrell_corp;
 
+USE tyrell_corp;
 -- Create table
-CREATE TABLE IF NOT EXISTS tyrell_corp.nexus6
+CREATE TABLE IF NOT EXISTS nexus6
 (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id),
-);
+        id INT UNIQUE AUTO_INCREMENT NOT NULL,
+        name VARCHAR(256) NOT NULL,
+        PRIMARY KEY (id));
 
 -- Insert an entry
-INSERT INTO `tyrell_corp.nexus6` (`name`) VALUES ("Emmastro");
+INSERT INTO nexus6 (name) VALUES ('Emmastro');
+
+REVOKE REPLICATION CLIENT ON *.* FROM 'holberton_user'@'localhost';
+GRANT SELECT ON *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'holberton_user'@'localhost';
