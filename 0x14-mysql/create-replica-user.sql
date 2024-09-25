02cf8eb08a8a40646bf26d46b8d80838dc7d10b2
Shiftyshadows <purple.quasar@proton.me>
2024-09-25 06:21:03 +0300

    3. Quite an experience to live in fear, isn't it?

--- 0x14-mysql/create-replica-user.sql
+++ 0x14-mysql/create-replica-user.sql
@@ -1,8 +1,10 @@
--- Create user to check DB status 
--- Run on the master server
-
-CREATE USER IF NOT EXISTS 'replica_user'@'%'
-IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT  REPLICATION CLIENT, SELECT ON *.*
-TO 'replica_user'@'%';
+-- Create replica_user with replication permissions
+CREATE USER IF NOT EXISTS 'replica_user'@'%'IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT  REPLICATION SLAVE, REPLICATION CLIENT  ON *.* TO 'replica_user'@'%';
 FLUSH PRIVILEGES;
+
+-- Grant SELECT permissions to holberton_user on mysql.user
+GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
+
+SELECT user, host FROM mysql.user WHERE user = 'replica_user';
